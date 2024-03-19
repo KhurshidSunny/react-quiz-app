755aae643dccfef588730dd2c990006cebd0dfb0
Khurshid Khan <khurshidsunny45@gmail.com>
2024-03-19 16:11:32 +0500

    header and main componenets are done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
+import Main from "./Main";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>questions?</p>
+      </Main>
     </div>
   );
 }
-
-export default App;
