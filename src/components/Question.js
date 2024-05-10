bda23ab61725cf2a964269a72fdf3b051391a2e9
Khurshid Khan <khurshidsunny45@gmail.com>
2024-05-10 20:44:43 +0500

    fixed a bug

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,10 +3,11 @@ import Options from "./Options";
 
 function Question() {
   const { questions, index } = useQuiz();
-  const question = questions[index];
+  const question = questions.at(index);
   return (
     <div>
-      <h4>{questions?.question}</h4>
+      <h4>{question.question}</h4>
+
       <Options question={question} />
     </div>
   );
